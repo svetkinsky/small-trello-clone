cfb0910a40b396eb94806aec3c9e9de159a3cb99
svetkinsky <tebsv@inbox.ru>
2020-03-22 20:17:25 +0300

    server post request to create record (new task and new column) to DB

--- stub/index.js
+++ stub/index.js
@@ -4,6 +4,7 @@ const path = require('path')
 const fs = require('fs')
 const mongoose = require("mongoose")
 const Schema = mongoose.Schema
+const jsonParser = express.json()
 
 
 
@@ -11,31 +12,57 @@ const app = express()
 const PORT = process.env.PORT || 3000
 
 
-const tasksSchema = new Schema({
-    id: Number,
-    idParend: Number,
-    text: String
+const dataSchema = new Schema({
+    idTask: Number,
+    contentTask: String,
+    idColumn: Number,
+    titleColumn: String
 })
+const Data = mongoose.model("Data", dataSchema)
+
 
 mongoose.connect("mongodb://localhost:27017/usersdb", {
     useUnifiedTopology: true,
     useNewUrlParser: true
 })
 
-const Task = mongoose.model("Tasks", tasksSchema)
-const task = new Task({
-    id: 10,
-    idParend: 36,
-    text: 'abcd'
-});
+app.use(express.static(__dirname + '/dist'))
 
-task.save(function (err) {
-    if (err) return console.log(err)
-    console.log("Сохранен объект", task)
+app.post('/create', jsonParser, (req, res) => {
+    if (!req.body) return res.sendStatus(400)
+
+    if (req.body.idTask) {
+        Data.create({
+            idTask: req.body.idTask,
+            contentTask: req.body.contentTask,
+            idColumn: req.body.idColumn
+        }, (err, data) => {
+            if (err) return console.log(err)
+            res.send(data)
+            console.log('Сохранена задача', data)
+        })
+    } else {
+        Data.create({
+            idColumn: req.body.idColumn,
+            titleColumn: req.body.titleColumn
+        }, (err, data) => {
+            if (err) return console.log(err)
+            res.send(data)
+            console.log('Сохранена колонка', data)
+        })
+    }
 })
 
 
-app.use(express.static(__dirname + '/dist'))
+
+
+// task.save(function (err) {
+//     if (err) return console.log(err)
+//     console.log("Сохранен объект", task)
+// })
+
+
+
 
 app.get('/tasks', (rq, rs) => {
     const filePath = path.resolve(__dirname, './response.json')
@@ -50,12 +77,12 @@ app.get('/tasks', (rq, rs) => {
 })
 
 app.get('/test', (rq, rs) => {
-    Task.find({}, function(err, docs){         
-        if(err) return console.log(err)
+    Task.find({}, function (err, docs) {
+        if (err) return console.log(err)
         rs.send(docs)
         console.log(docs)
     });
-  
+
 })
 
 app.listen(PORT, (error) => {
